2b21ee50569ceaa641686e3cf5ffcab0da554e59
Casey Calkins <a.caseycalkins@outlook.com>
2024-03-26 14:36:04 -0500

    run black

--- neighborhood_watch/views.py
+++ neighborhood_watch/views.py
@@ -3,11 +3,13 @@ import logging
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.authentication import (SessionAuthentication,
+                                           TokenAuthentication)
 from rest_framework.authtoken.models import Token
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.response import Response
+from rest_framework.decorators import (api_view, authentication_classes,
+                                       permission_classes)
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
 
